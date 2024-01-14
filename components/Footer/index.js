c796676939181b6dacb266f3f4d4962819cbc539
jgache <jgache1@my.bcit.ca>
2024-01-13 19:13:03 -0800

    instagram uploud v1

---
+++ components/Footer/index.js
@@ -0,0 +1,34 @@
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return(
+        
+        <footer className={styles.footer}>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+
+<div class="footer-container">
+    <span class="footer-text">Meta </span>
+    <span class="footer-text">About </span>
+    <span class="footer-text">Blog </span>
+    <span class="footer-text">Jobs </span>
+    <span class="footer-text">Help </span>
+    <span class="footer-text">API</span>
+    <span class="footer-text">Privacy</span>
+    <span class="footer-text">Terms</span>
+    <span class="footer-text">Locations</span>
+    <span class="footer-text">Instagram Lite</span>
+    <span class="footer-text">Threads</span>
+    <span class="footer-text">Contact Uploading & Non-Users</span>
+    <span class="footer-text">Meta Verified</span>
+</div> 
+
+<div class="under-text-footer">
+    <span class="footer-text">English</span>
+    <span class="footer-text">© 2022 Instagram from Jerome Gache</span>
+    
+</div>
+        </footer>
+
+    )
+    
+}
